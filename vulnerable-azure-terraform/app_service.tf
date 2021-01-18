4ba58cb45ddbebffbbf390d36625737ccb9b0c5d
Dan Gora <daniel.gora@cloudreach.com>
2021-01-18 11:22:39 +0000

    Adjust code for demo purposes

--- vulnerable-azure-terraform/app_service.tf
+++ vulnerable-azure-terraform/app_service.tf
@@ -12,7 +12,7 @@ resource azurerm_app_service_plan "appplan" {
 resource azurerm_app_service "app-service1" {
   app_service_plan_id = azurerm_app_service_plan.appplan.id
   location            = var.location
-  name                = "app-service-${var.environment}${random_integer.rnd_int.result}"
+  name                = "dg-app-service-${var.environment}${random_integer.rnd_int.result}"
   resource_group_name = azurerm_resource_group.vbdgroup.name
   https_only          = false
   site_config {
@@ -23,7 +23,7 @@ resource azurerm_app_service "app-service1" {
 resource azurerm_app_service "app-service2" {
   app_service_plan_id = azurerm_app_service_plan.appplan.id
   location            = var.location
-  name                = "app-service-${var.environment}${random_integer.rnd_int.result}"
+  name                = "dg-app-service-${var.environment}${random_integer.rnd_int.result}"
   resource_group_name = azurerm_resource_group.vbdgroup.name
   https_only          = true
 
